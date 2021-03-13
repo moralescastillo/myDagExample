dff1311b1c5e330163428d84d3d4f580af177135
moralescastillo <moralescas@gmail.com>
2021-03-13 20:26:38 +0100

    try pythonOperator function context

--- bash_python_dag_example.py
+++ bash_python_dag_example.py
@@ -13,6 +13,11 @@ def print_current_timestamp():
     
     return print(datetime.today())
 
+def print_context(ds, **kwargs):
+    pprint(kwargs)
+    print(ds)
+    return 'Whatever you return gets printed in the logs'
+
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -21,20 +26,23 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
   }
 
-dag = DAG('bash_python_dag_example', 
+dag = DAG('send_email_morning', 
           default_args=default_args,
           schedule_interval= '0 0 * * *')
 
 t1 = BashOperator(
-    task_id='hello_world_bash',
+    task_id='hello_world_print',
     bash_command='printf "hello world"',
     dag=dag)
 
 t2 = PythonOperator(
-    task_id = 'current_timestamp_python',
-    python_callable = print_current_timestamp,
+    task_id = 'current_timestamp_print',
+    python_callable = print_context,
     dag=dag)
 
 ## dependencies
 
 t1 >> t2
+
+
+print_current_timestamp()
